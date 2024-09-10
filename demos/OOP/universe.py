23ab57b9b8f4dbddd5233442b5355859c77cb737
p-jab <pete.b@o2.pl>
2024-09-10 14:45:00 +0100

    Seminar week 10 (OOP Tasks)

--- demos/OOP/universe.py
+++ demos/OOP/universe.py
@@ -12,18 +12,18 @@ class Universe:
         n_humans = randint(1,10)
         n_robots = randint(1,10)
         for i in range(n_humans):
-            h = Human()
+            h = Human(f"H{i}")
             p.add_human(h)
         for i in range(n_robots):
-            r = Robot()
+            r = Robot(f"R{i}")
             p.add_robot(r)
         self.planets.append(p)
 
 if __name__ == "__main__":
     u = Universe()
-    p1 = Planet()
+    p1 = Planet([],[],"Alpha")
     u.generate(p1)
-    p2 = Planet()
+    p2 = Planet([],[], "Beta")
     u.generate(p2)
-    for item in u.planets:
-        print(item)
+    for i in u.planets:
+        print(i)
